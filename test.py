b8c506d9fec79fb047bed1e5195ddb4e4244cdb4
ahmedtawfiiiq <ahmedtawfiiiq24@gmail.com>
2024-10-12 02:23:03 +0300

    update

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-X = np.array([[0, 1], [1, 0]])
-Y = np.array([[0, -1j], [1j, 0]])
-Z = np.array([[1, 0], [0, -1]])
-H = np.array([[1, 1], [1, -1]])
-S = np.array([[1, 0], [0, 1j]])
-T = np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]])
+X = np.array([[0, 1], [1, 0]], dtype=complex)
+Y = np.array([[0, -1j], [1j, 0]], dtype=complex)
+Z = np.array([[1, 0], [0, -1]], dtype=complex)
+H = np.array([[1, 1], [1, -1]], dtype=complex) / np.sqrt(2)
+S = np.sqrt(Z)
+T = np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]], dtype=complex)
+V = np.sqrt(X)
+Rz = lambda theta: np.array([[np.exp(-1j * theta / 2), 0], [0, np.exp(1j * theta / 2)]], dtype=complex)
 
-# U = Y @ Z
-# print(U)
-# print(np.allclose(U, 1j * X))
-
-print(np.linalg.inv(H))
+print(X)
+print(np.allclose(np.power(V, 2), X))
